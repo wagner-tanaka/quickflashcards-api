38418d2f3e8f0e4c919c6df53bb59aa6e7faae91
wagner <wbtanaka@gmail.com>
2023-11-30 18:02:08 +0900

    [DEV]:: TEST - Login , improvements, added setUp and DataProvider

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -10,23 +10,77 @@ class LoginTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testLoginSuccess(): void
-    {
-        // create user
-        $user = User::factory()->create();
+    private User $user;
+    private string $correctPassword = 'password';
 
-        // login
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create([
+            'password' => bcrypt($this->correctPassword),
+        ]);
+    }
+
+    public static function userLoginDataProvider(): array
+    {
+        return [
+            'with correct password' => ['password', 200],
+            'with incorrect password' => ['wrongPassword', 401],
+        ];
+    }
+
+    public static function malformedEmailProvider(): array
+    {
+        return [
+            'missing domain' => ['email@'],
+            'missing at symbol' => ['email'],
+        ];
+    }
+
+    /**
+     * @dataProvider userLoginDataProvider
+     */
+    public function testUserLogin(string $password, int $expectedStatus)
+    {
         $response = $this->postJson('/api/auth/login', [
-            'email' => $user->email,
-            'password' => 'password',
+            'email' => $this->user->email,
+            'password' => $password,
         ]);
 
-        // assert response status and structure
-        $response
-            ->assertStatus(200)
-            ->assertJsonStructure([
-                'access_token',
-                'token_type',
-            ]);
+        $response->assertStatus($expectedStatus);
+    }
+
+    public function testLoginWithInvalidEmail()
+    {
+        $response = $this->postJson('/api/auth/login', [
+            'email' => 'nonexistent@example.com',
+            'password' => $this->correctPassword,
+        ]);
+
+        $response->assertStatus(401);
+    }
+
+    public function testLoginWithEmptyCredentials()
+    {
+        $response = $this->postJson('/api/auth/login', [
+            'email' => '',
+            'password' => '',
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    /**
+     * @dataProvider malformedEmailProvider
+     */
+    public function testLoginWithMalformedEmail(string $malformedEmail)
+    {
+        $response = $this->postJson('/api/auth/login', [
+            'email' => $malformedEmail,
+            'password' => $this->correctPassword,
+        ]);
+
+        $response->assertStatus(422);
     }
 }
